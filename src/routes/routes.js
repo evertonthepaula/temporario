719345a5ed5ec6ee04a152b85759875c5d616bab
evertonthepaula <evertothepaula@gmail.com>
2020-01-28 23:45:49 -0300

    Corrigir rota

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Dashboard from '../pages/dashboard';
+import Dashboard from '../pages/Dashboard/index';
 
 export default () => (
   <BrowserRouter>
