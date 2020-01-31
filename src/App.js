b45457f0d119fa965cba7071c954be06bb880bb4
evertonthepaula <evertothepaula@gmail.com>
2020-01-31 12:16:01 -0300

    Configurar rotas privadas

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import './config/ReactotronConfig';
 
 import store from './store';
-import Routes from './routes/routes';
+import Routes from './routes';
 import GloalStyle from './styles/global';
 
 export default () => (
---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Router, Switch } from 'react-router-dom';
+
+import Route from './Route';
+import history from '../services/history';
+
+import Dashboard from '../pages/Dashboard';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Profile from '../pages/Profile';
+import Landing from '../pages/Landing';
+
+export default () => (
+  <Router history={history}>
+    <Switch>
+      <Route path="/" component={SignIn} exact />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/perfil" component={Profile} isPrivate />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/Landing" component={Landing} />
+
+      <Route path="/" component={() => <p>404</p>} />
+    </Switch>
+  </Router>
+);
